22fdf94d5f80c996f72ed292e45c157c20e049c3
Arseniy Kuznetsov <Kuarni22@gmail.com>
2023-03-25 20:31:49 +0300

    init(class): RBTree feat: class RBNode

---
+++ src/main/kotlin/RBTree.kt
@@ -0,0 +1,35 @@
+class RBTree<Key : Comparable<Key>, Value> : BalanceTree<Key, Value>() {
+    protected class RBNode<Key : Comparable<Key>, Value>(
+        key: Key,
+        value: Value,
+        parent: RBNode<Key, Value>?= null,
+        left: RBNode<Key, Value>? = null,
+        right: RBNode<Key, Value>? = null,
+        var Black: Boolean = false
+    ) : Node<Key, Value>(key, value, parent, left, right) {
+        fun swapColor() {
+            Black = !Black
+        }
+    }
+
+    override fun insert(key: Key, value: Value) {
+        if (rootNode == null)
+            rootNode = RBNode(key, value)
+    }
+
+    override fun rebalancing() {
+        TODO("Not yet implemented")
+    }
+
+    override fun leftRotation() {
+        TODO("Not yet implemented")
+    }
+
+    override fun rightRotation() {
+        TODO("Not yet implemented")
+    }
+
+    override fun remove(key: Key) {
+        TODO("Not yet implemented")
+    }
+}
