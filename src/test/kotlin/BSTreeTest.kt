cfdf0fa4317925cdeefeab1c59371c4cb94391fc
Arseniy Kuznetsov <Kuarni22@gmail.com>
2023-03-28 02:53:12 +0300

    feat: tests are allocated a separate class

--- src/test/kotlin/BSTreeTest.kt
+++ src/test/kotlin/BSTreeTest.kt
@@ -1,13 +1,26 @@
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class BSTreeTest {
-    @Test
-    fun addFirstNodeCorrectTest() {
-        val tree = BSTree<Int, String>()
-        val expected = "4k"
-        tree.insert(4, "4k")
-        Assertions.assertEquals(expected, tree.get(4))
+    @Nested
+    inner class `Test add` {
+        fun generateTreeWithAdd(vararg arr: Int): BinTree<Int, String> {
+            val tree = BSTree<Int, String>()
+            for (i in arr)
+                tree.insert(i, "${i}k")
+            return tree
+        }
+
+        @Test
+        fun `add one node test`() {
+            val expected = "4k"
+            assertEquals(expected, generateTreeWithAdd(4).get(4))
+        }
+
+        @Test
+        fun `add three nodes test`() {
+            assertEquals("2 \n1 3 \n", generateTreeWithAdd(2, 1, 3).Debug().treeKeysInString())
+        }
     }
 }
