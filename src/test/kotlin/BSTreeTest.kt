c3524161d15c00550fc669d9923824cdde38f6fa
Arseniy Kuznetsov <Kuarni22@gmail.com>
2023-03-28 03:20:50 +0300

    feat: add tests (insert tests)

--- src/test/kotlin/BSTreeTest.kt
+++ src/test/kotlin/BSTreeTest.kt
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test
 
 class BSTreeTest {
     @Nested
-    inner class `Test add` {
-        fun generateTreeWithAdd(vararg arr: Int): BinTree<Int, String> {
+    inner class `Test insert` {
+        fun generateTreeWithInsert(vararg arr: Int): BinTree<Int, String> {
             val tree = BSTree<Int, String>()
             for (i in arr)
                 tree.insert(i, "${i}k")
@@ -13,14 +13,44 @@ class BSTreeTest {
         }
 
         @Test
-        fun `add one node test`() {
+        fun `insert one node test`() {
             val expected = "4k"
-            assertEquals(expected, generateTreeWithAdd(4).get(4))
+            assertEquals(expected, generateTreeWithInsert(4).get(4))
         }
 
         @Test
-        fun `add three nodes test`() {
-            assertEquals("2 \n1 3 \n", generateTreeWithAdd(2, 1, 3).Debug().treeKeysInString())
+        fun `insert three nodes test`() {
+            assertEquals("2 \n1 3 \n", generateTreeWithInsert(2, 1, 3).Debug().treeKeysInString())
+        }
+
+        @Test
+        fun `degenerate tree`() {
+            assertEquals("1 \n- 2 \n- - - 3 \n", generateTreeWithInsert(1, 2, 3).Debug().treeKeysInString())
+        }
+
+        @Nested
+        inner class `Equal key` {
+            @Test
+            fun `two inserts of the first node`() {
+                val tree = generateTreeWithInsert(4)
+                tree.insert(4, "5k")
+                assertEquals("5k", tree.get(4))
+            }
+
+            @Test
+            fun `two inserts of the first node in non-degenerate tree`() {
+                val tree = generateTreeWithInsert(4, 1, 5, 6)
+                tree.insert(4, "5k")
+                assertEquals("5k", tree.get(4))
+            }
+
+            @Test
+            fun `two inserts of node`() {
+                val tree = generateTreeWithInsert(5, 6, 4)
+                tree.insert(4, "5k")
+                assertEquals("5 \n4 6 \n", tree.Debug().treeKeysInString())
+                assertEquals("5k", tree.get(4))
+            }
         }
     }
 }
