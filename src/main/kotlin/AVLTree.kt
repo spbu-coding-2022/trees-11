29b61c5c8fed37fd8458afb3c4cd71d0bc2bc367
Owner <email>
2023-04-10 22:55:44 +0300

    feat: balancing is implemented

--- src/main/kotlin/AVLTree.kt
+++ src/main/kotlin/AVLTree.kt
@@ -1,5 +1,5 @@
 class AVLTree<Key : Comparable<Key>, Value> : BalanceTree<Key, Value>() {
-    protected class AVLNode<Key : Comparable<Key>, Value>(
+    private class AVLNode<Key : Comparable<Key>, Value>(
         key: Key,
         value: Value,
         parent: AVLNode<Key, Value>? = null,
@@ -8,50 +8,78 @@ class AVLTree<Key : Comparable<Key>, Value> : BalanceTree<Key, Value>() {
         var height: UByte = 0U
     ) : BinNode<Key, Value>(key, value, parent, left, right)
 
-    private fun createNode(key: Key, value: Value) {
-        val node = AVLNode<key, value>
-        return node
-    }
-
-    private fun rotationAVL(parentKey: Key, type: RotationType): BinNode<Key, Value>? {
-        val node = rotation(parentKey, type)
-
-        node.height = max(node.left.height, node.right.height) + 1
-        node.parent.height = max(node.parent.left.height, node.parent.right.height) + 1
-
-        return node
-    }
-
     override fun insert(key: Key, value: Value) {
-        val node = createNode(key, value)
-
-        if (key < node.key) node.left = insert(node.left, key)
-        else if (key > node.key) node.right = insert(node.right, key)
-        else return node
-
-        node.height = 1 + (max(node.left.height, node.left.right))
-
-        val heightDifference = node.left.height - node.right.height
-
-        if (heightDifference > 1 and key < node.left.key) {
-            return rotationAVL(parent.key, RotationType.right)
-        }
-        if (heightDifference < -1 and key > node.right.key) {
-            return rotationAVL(parent.key, RotationType.left)
-        }
-        if (heightDifference > 1 and key > node.left.key) {
-            node.left = rotationAVL(parent.key, RotationType.left)
-            return rotationAVL(parent.key, RotationType.right)
-        }
-        if (heightDifference < -1 and key < node.right.key) {
-            node.right = rotationAVL(parent.key, RotationType.right)
-            return rotationAVL(parent.key, RotationType.left)
-        }
-
-        return node
+        if (rootNode != null) {
+            if ((rootNode as AVLNode).height != 255.toUByte()) {
+                insertService(AVLNode(key, value))
+            }
+        } else
+            rootNode = AVLNode(key, value)
+        balance(rootNode as AVLNode)
     }
-
     override fun remove(key: Key) {
         TODO("Not yet implemented")
+    }
+    private fun balance(node: AVLNode<Key, Value>) {
+        if (node.left != null && node.right != null) {
+            if ((node.left as AVLNode).height > (node.right as AVLNode).height + 1.toUByte()) {
+                if ((node.left!!.left as AVLNode).height >= (node.left!!.right as AVLNode).height) {
+                    rotateRight(node)
+                } else {
+                    rotateLeft(node.left!! as AVLNode<Key, Value>)
+                    rotateRight(node)
+                }
+            } else if ((node.right as AVLNode).height > (node.left as AVLNode).height + 1.toUByte()) {
+                if ((node.right!!.right as AVLNode).height >= (node.right!!.left as AVLNode).height) {
+                    rotateLeft(node)
+                } else {
+                    rotateRight(node.right!! as AVLNode<Key, Value>)
+                    rotateLeft(node)
+                }
+            }
+            updateHeight(node)
+            if (node.parent != null) {
+                balance(node.parent!! as AVLNode<Key, Value>)
+            }
+        }
+    }
+    private fun updateHeight(node: AVLNode<Key, Value>) {
+        val left = node.left?.let { (it as AVLNode).height } ?: 0U
+        val right = node.right?.let { (it as AVLNode).height } ?: 0U
+        node.height = (maxOf(left, right) + 1.toUByte()).toUByte()
+    }
+    private fun rotateLeft(node: AVLNode<Key, Value>) {
+        val child = node.right!!
+        child.parent = node.parent
+        if (node.parent == null) {
+            rootNode = child
+        } else if (node.parent!!.left == node) {
+            node.parent!!.left = child
+        } else {
+            node.parent!!.right = child
+        }
+        node.right = child.left
+        child.left?.parent = node
+        node.parent = child
+        child.left = node
+        updateHeight(node)
+        updateHeight(child as AVLNode<Key, Value>)
+    }
+    private fun rotateRight(node: AVLNode<Key, Value>) {
+        val child = node.left!!
+        child.parent = node.parent
+        if (node.parent == null) {
+            rootNode = child
+        } else if (node.parent!!.left == node) {
+            node.parent!!.left = child
+        } else {
+            node.parent!!.right = child
+        }
+        node.left = child.right
+        child.right?.parent = node
+        node.parent = child
+        child.right = node
+        updateHeight(node)
+        updateHeight(child as AVLNode<Key, Value>)
     }
 }
