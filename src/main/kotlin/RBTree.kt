8dd2afa63fa06cac28618f2e7e9c888dd9807e9e
juliakononov <julickk04@mail.ru>
2023-04-24 03:18:35 +0300

    fix: RBTree balancer after remove

--- src/main/kotlin/RBTree.kt
+++ src/main/kotlin/RBTree.kt
@@ -83,7 +83,6 @@ open class RBTree<Key : Comparable<Key>, Value> : BalanceTree<Key, Value> {
 
         //root color should always be black
         if (parent == null) (rootNode as RBNode?)?.color = BLACK
-
         else if (parent.color == BLACK) return
         else {
             val uncle = getUncle(node)
@@ -114,60 +113,149 @@ open class RBTree<Key : Comparable<Key>, Value> : BalanceTree<Key, Value> {
         var node = removeNode
 
         while ((node != rootNode) && (node?.color == BLACK)) {
-            var brother = getSibling(node)
+            val parent = node.parent as RBNode<Key, Value>?
+            val brother = getSibling(node) ?: error("remove error: brother must exist")
+
             //balancing when a node is the left child of its parent
-            if (node == node.parent?.left) {
-                if (brother?.color == RED) {
-                    (node.parent as RBNode<Key, Value>?)?.swapColor()
-                    brother.swapColor()
-                    rotation(node.parent, RotationType.LEFT)
-                    brother = (node.parent as RBNode<Key, Value>?)?.right as RBNode<Key, Value>?
-                }
-                if (((brother?.left == null) || (brother.left as RBNode<Key, Value>?)?.color == BLACK) &&
-                    ((brother?.right == null) || (brother.right as RBNode<Key, Value>?)?.color == BLACK)) {
-                    brother?.color = RED
-                    node = node.parent as RBNode<Key, Value>
-                } else {
-                    if ((brother.right == null) || (brother.right as RBNode<Key, Value>?)?.color == BLACK) {
-                        (brother.left as RBNode<Key, Value>?)?.color = BLACK
-                        brother.color = RED
-                        rotation(brother, RotationType.RIGHT)
-                        brother = node.parent?.right as RBNode<Key, Value>?
+            if (node == parent?.left) {
+
+                //if the parent color is red, the brother's color must be black
+                if (parent.color == RED) {
+
+                    //case when brother has a red child
+                    if (((brother.left as RBNode<Key, Value>?)?.color == RED) ||
+                        ((brother.right as RBNode<Key, Value>?)?.color == RED)) {
+                        parent.color = BLACK
+                        if ((brother.left as RBNode<Key, Value>?)?.color == RED) {
+                            rotation(brother, RotationType.RIGHT)
+                        } else {
+                            brother.color = RED
+                            (brother.right as RBNode<Key, Value>?)?.color = BLACK
+                        }
+                        node = rotation(parent, RotationType.LEFT) as RBNode<Key, Value>?
+                    } else {
+                        brother.swapColor()
+                        parent.swapColor()
                     }
-                    brother?.color = (node.parent as RBNode<Key, Value>).color
-                    (node.parent as RBNode<Key, Value>).color = BLACK
-                    (brother?.right as RBNode<Key, Value>).color = BLACK
-                    rotation(node.parent as RBNode<Key, Value>?, RotationType.LEFT)
-                    node = rootNode as RBNode<Key, Value>
+                    break
+                }
+
+                else if (brother.color == RED) {
+                    //brother's left child must exist and his color must be black
+                    var brotherLeftChild =
+                        brother.left as RBNode<Key, Value>? ?: error("remove error: brother's left child must exist")
+
+                    if (((brotherLeftChild.left as RBNode<Key, Value>?)?.color == RED) ||
+                        ((brotherLeftChild.right as RBNode<Key, Value>?)?.color == RED)) {
+                        if ((brotherLeftChild.left as RBNode<Key, Value>?)?.color == RED) {
+                            brotherLeftChild.swapColor()
+                            (brotherLeftChild.left as RBNode<Key, Value>?)?.swapColor()
+                            brotherLeftChild = rotation(brotherLeftChild, RotationType.RIGHT) as RBNode<Key, Value>
+                        }
+                        (brotherLeftChild.right as RBNode<Key, Value>?)?.swapColor()
+                        rotation(brother, RotationType.RIGHT)
+                    }
+
+                    else {
+                        brother.swapColor()
+                        brotherLeftChild.swapColor()
+                    }
+                    rotation(parent, RotationType.LEFT) as RBNode<Key, Value>?
+                    break
+                }
+
+                //if brother's color is black
+                else {
+                    if (((brother.left == null) || (brother.left as RBNode<Key, Value>?)?.color == BLACK) &&
+                        ((brother.right == null) || (brother.right as RBNode<Key, Value>?)?.color == BLACK)) {
+                        brother.color = RED
+                        node = parent
+                    }
+
+                    else {
+                        if ((brother.right == null) || (brother.right as RBNode<Key, Value>?)?.color == BLACK) {
+                            (brother.left as RBNode<Key, Value>?)?.color = BLACK
+                            rotation(brother, RotationType.RIGHT)
+                        }
+                        else {
+                            (brother.right as RBNode<Key, Value>?)?.color = BLACK
+                        }
+                        rotation(parent, RotationType.LEFT) as RBNode<Key, Value>?
+                        break
+                    }
                 }
             }
+
             //balancing when a node is the right child of its parent
             else {
-                if (brother?.color == RED) {
-                    (node.parent as RBNode<Key, Value>?)?.swapColor()
-                    brother.swapColor()
-                    rotation(node.parent, RotationType.RIGHT)
-                    brother = (node.parent as RBNode<Key, Value>?)?.left as RBNode<Key, Value>?
-                }
-                if (((brother?.left == null) || (brother.left as RBNode<Key, Value>?)?.color == BLACK) &&
-                    ((brother?.right == null) || (brother.right as RBNode<Key, Value>?)?.color == BLACK)) {
-                    brother?.color = RED
-                    node = node.parent as RBNode<Key, Value>
-                } else {
-                    if ((brother.left == null) || (brother.left as RBNode<Key, Value>?)?.color == BLACK) {
-                        (brother.right as RBNode<Key, Value>?)?.color = BLACK
-                        brother.color = RED
-                        rotation(brother, RotationType.LEFT)
-                        brother = node.parent?.left as RBNode<Key, Value>?
+
+                //if the parent color is red, the brother's color must be black
+                if (parent?.color == RED) {
+
+                    //case when brother has a red child
+                    if (((brother.left as RBNode<Key, Value>?)?.color == RED) ||
+                        ((brother.right as RBNode<Key, Value>?)?.color == RED)) {
+                        parent.color = BLACK
+                        if ((brother.right as RBNode<Key, Value>?)?.color == RED) {
+                            rotation(brother, RotationType.LEFT)
+                        } else {
+                            brother.color = RED
+                            (brother.left as RBNode<Key, Value>?)?.color = BLACK
+                        }
+                        node = rotation(parent, RotationType.RIGHT) as RBNode<Key, Value>?
                     }
-                    brother?.color = (node.parent as RBNode<Key, Value>).color
-                    (node.parent as RBNode<Key, Value>).color = BLACK
-                    (brother?.left as RBNode<Key, Value>).color = BLACK
-                    rotation(node.parent as RBNode<Key, Value>?, RotationType.RIGHT)
-                    node = rootNode as RBNode<Key, Value>
+
+                    else {
+                        brother.swapColor()
+                        parent.swapColor()
+                    }
+                    break
+                }
+
+                else if (brother.color == RED) {
+                    //brother's right child must exist and his color must be black
+                    var brotherRightChild =
+                        brother.right as RBNode<Key, Value>? ?: error("remove error: brother's right child must exist")
+
+                    if (((brotherRightChild.left as RBNode<Key, Value>?)?.color == RED) ||
+                        ((brotherRightChild.right as RBNode<Key, Value>?)?.color == RED)
+                    ) {
+                        if ((brotherRightChild.right as RBNode<Key, Value>?)?.color == RED) {
+                            brotherRightChild.swapColor()
+                            (brotherRightChild.right as RBNode<Key, Value>?)?.swapColor()
+                            brotherRightChild = rotation(brotherRightChild, RotationType.LEFT) as RBNode<Key, Value>
+                        }
+                        (brotherRightChild.left as RBNode<Key, Value>?)?.swapColor()
+                        rotation(brother, RotationType.LEFT)
+                    }
+
+                    else {
+                        brother.swapColor()
+                        brotherRightChild.swapColor()
+                    }
+                    rotation(parent, RotationType.RIGHT) as RBNode<Key, Value>?
+                    break
+                }
+
+                //if brother's color is black
+                else {
+                    if (((brother.left == null) || (brother.left as RBNode<Key, Value>?)?.color == BLACK) &&
+                        ((brother.right == null) || (brother.right as RBNode<Key, Value>?)?.color == BLACK)) {
+                        brother.color = RED
+                        node = parent
+                    } else {
+                        if ((brother.left == null) || (brother.left as RBNode<Key, Value>?)?.color == BLACK) {
+                            (brother.right as RBNode<Key, Value>?)?.color = BLACK
+                            rotation(brother, RotationType.LEFT)
+                        }
+                        else {
+                            (brother.left as RBNode<Key, Value>?)?.color = BLACK
+                        }
+                        rotation(parent, RotationType.RIGHT) as RBNode<Key, Value>?
+                        break
+                    }
                 }
             }
-            node.color = BLACK
         }
     }
 }
