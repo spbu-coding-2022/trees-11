0d3bbcc8ee2a6dec255e2fb2a1547fbdc6d588b4
juliakononov <julickk04@mail.ru>
2023-04-28 01:05:39 +0300

    feat: new dependencies (Compose, MaterialDesign)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,17 @@
 plugins {
     id("trees.kotlin-application-conventions")
+
+    id("org.jetbrains.compose") version "1.4.0"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
 
 val sqliteJdbcVersion: String by project
 
@@ -12,11 +20,10 @@ dependencies {
     implementation("org.neo4j.driver:neo4j-java-driver:5.7.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
     implementation("org.xerial", "sqlite-jdbc", sqliteJdbcVersion)
+
+    implementation(compose.desktop.currentOs)
+    implementation ("org.jetbrains.compose.material3:material3-desktop:1.2.1")
 }
-//
-//kotlin {
-//    jvmToolchain(8)
-//}
 
 application {
     mainClass.set("MainKt")
