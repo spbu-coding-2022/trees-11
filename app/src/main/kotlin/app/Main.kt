96aab0ef1c885ca2a5df8a135136d81822547f28
juliakononov <julickk04@mail.ru>
2023-05-03 01:55:33 +0300

    fix(app): icon

--- app/src/main/kotlin/app/Main.kt
+++ app/src/main/kotlin/app/Main.kt
@@ -5,13 +5,15 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import UIT.AppTheme
+import androidx.compose.ui.res.painterResource
 
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Trees",
-        state = rememberWindowState(width = 900.dp, height = 700.dp)
+        state = rememberWindowState(width = 900.dp, height = 700.dp),
+        icon = painterResource("icon.png")
     ) {
         AppTheme {
             Main(window)
